4d28ec1b116eeda7032ae1891ddd5d3226f9eb58
Riyazm16 <riyajmulla16296@gmail.com>
2023-11-26 11:25:56 +0530

    fix: code for contact

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,39 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
   return (
-    <nav className="font-sans fixed flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
-      <div
-        className="mb-2 sm:mb-0 flex flex-row
-        "
-      >
-        <div className="h-100 w-100 self-center mr-2">
-          <img className="h-100 w-100 self-center" src={LOGO_URL} />
-        </div>
-        <div>
-          <a
-            href="/home"
-            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold"
-          ></a>
-        </div>
+    <div className="header">
+      <a href="/">
+        <img
+          className="logo"
+          src={LOGO_URL}
+          alt="Food Fire Logo"
+          title="Food Fire"
+        />
+      </a>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <li>
+            <button
+              className="login-btn"
+              onClick={() => {
+                btnName === "Login"
+                  ? setBtnName("Logout")
+                  : setBtnName("Login");
+              }}
+            >
+              {btnName}
+            </button>
+          </li>
+        </ul>
       </div>
-
-      <div className="sm:mb-0 self-center">
-        <a
-          href="#"
-          className="text-md no-underline text-black hover:text-blue-dark ml-2 px-1"
-        >
-          Home
-        </a>
-        <a
-          href="#"
-          className="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1"
-        >
-          About us
-        </a>
-        <a
-          href="#"
-          className="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1"
-        >
-          Cart
-        </a>
-      </div>
-    </nav>
+    </div>
   );
 };
 
--- src/components/RestaturantCard.js
+++ src/components/RestaturantCard.js
@@ -10,36 +10,26 @@ const RestaturantCard = (props) => {
     sla: { slaString },
     costForTwo,
   } = resData?.info;
-  const divStyle = {
-    backgroundImage: `url(${CDN_URL}${cloudinaryImageId})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  };
+  const  backgroundImage= `${CDN_URL}${cloudinaryImageId}`;
   return (
-    <div class="flex items-center  justify-center   from-[#F9F5F3] via-[#F9F5F3] to-[#F9F5F3] bg-gradient-to-br px-2 :hover cursor-pointer ">
-      <div class="w-full max-w-md mt-32 mx-auto bg-white rounded-3xl shadow-xl overflow-hidden  hover:shadow-indigo-500/40">
-        <div class="max-w-md mx-auto">
-          <div className="h-[236px]" style={divStyle}></div>
-          <div className="p-4 sm:p-6">
-            <p className="font-bold text-gray-700 text-[22px] leading-7 mb-1">
-              {name}
-            </p>
-            <p className="text-[17px] font-bold text-[#0FB478]">{costForTwo}</p>
-            <p className="text-[#7C7C80] font-[15px] mt-6">
-              {cuisines.join(", ")}
-            </p>
-            <a
-              target="_blank"
-              href="foodiesapp://food/1001"
-              className="block mt-10 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform bg-[#FFC933] rounded-[14px] hover:bg-[#FFC933DD] focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80"
-            >
-              {slaString}
-            </a>
-            <Ratings ratings={avgRating} key={`${name}${costForTwo}`} />
-          </div>
-        </div>
-      </div>
-    </div>
+    <div className="card">
+    <img src={backgroundImage} />
+    <h2>{name}</h2>
+    <h5>{cuisines.join(", ")}</h5>
+    {/* <h6>{area}</h6> */}
+    <span>
+      <h4
+        style={
+          avgRating < 4 ? { backgroundColor: "red" } : { color: "white" }
+        }
+      >
+        <i className="fa-solid fa-star"></i>
+        {avgRating}
+      </h4>
+      <h4>{slaString}</h4>
+      <h4>{    costForTwo}</h4>
+    </span>
+  </div>
   );
 };
 
